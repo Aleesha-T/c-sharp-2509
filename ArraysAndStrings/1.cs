9a6ba5b0424fc6cf70d99d1f7ba16f349bb6e813
Aleesha-T <aleesha.alwiland@gmail.com>
2024-11-01 21:51:17 +0530

    Commit

---
+++ ArraysAndStrings/1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndStrings
+{
+    public class Matrix
+    {
+        public static int[,] GetMatrix(int[,] matrix, int rows, int columns)
+        {
+            Console.WriteLine("Enter elements");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            return matrix; 
+        }
+        //Gets matrix from user.
+        public static void DisplayMatrix(int[,] matrix, int rows, int columns)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        //Displays matrix.
+        public static int[,] FindSum(int[,] sumMatrix, int[,] firstMatrix, int[,] secondMatrix, int rows, int columns)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    sumMatrix[i, j] = firstMatrix[i, j] + secondMatrix[i, j];
+                }
+            }
+            return sumMatrix;
+        }
+        //Finds sum of matrices.
+        public static int[,] FindProduct(int[,] productMatrix, int[,] firstMatrix, int[,] secondMatrix, 
+                                          int rows, int columns_Second, int columns_First)
+        {
+            int temp = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns_Second; j++)
+                {
+                    for (int k = 0; k < columns_First; k++)
+                    {
+                        temp = temp + firstMatrix[i, k] * secondMatrix[k, j];
+                    }
+                    productMatrix[i, j] = temp;
+                    temp = 0;
+                }
+            }
+            return productMatrix;
+        }
+        //Finds product of matrices.
+        public static int[,] FindTranspose(int[,] transposeMatrix, int[,] firstMatrix, int rows, int columns)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    transposeMatrix[j, i] = firstMatrix[i, j];
+                }
+            }
+            return transposeMatrix;
+        }
+        //Finds transpose matrix.
+    }
+}
--- ArraysAndStrings/Program.cs
+++ ArraysAndStrings/Program.cs
@@ -13,128 +13,89 @@ namespace ArraysAndStrings
             ////Array Manipulations
             ////Assignment 1.Write C# programs on Matrix addition, Matrix Multiplication and Matrix Transpose. 
 
-            Console.WriteLine("Enter 1 for addition, 2 for multiplication, 3 for transpose");
-            int choice = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter 1 for addition, 2 for multiplication, 3 for transpose");
+            //int choice = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter no. of rows of first matrix");
-            int rows_firstMatrix = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter no. of columns of first matrix");
-            int columns_firstMatrix = Convert.ToInt32(Console.ReadLine());
-            int[,] firstMatrix = new int[rows_firstMatrix, columns_firstMatrix];
-            int[,] secondMatrix = null;
-            Console.WriteLine("Enter elements");
-            for (int i = 0; i < rows_firstMatrix; i++)
-            {
-                for (int j = 0; j < columns_firstMatrix; j++)
-                {
-                    firstMatrix[i, j] = Convert.ToInt32(Console.ReadLine());
-                }
-            }
+            //Console.WriteLine("Enter no. of rows of first matrix");
+            //int rows_firstMatrix = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter no. of columns of first matrix");
+            //int columns_firstMatrix = Convert.ToInt32(Console.ReadLine());
+            ////Gets rows and columns.
+            //int[,] firstMatrix = new int[rows_firstMatrix, columns_firstMatrix];
+            //int[,] secondMatrix = null;
+           
+            //firstMatrix=Matrix.GetMatrix(firstMatrix, rows_firstMatrix, columns_firstMatrix);
+            ////Gets matrix.
 
-            if (choice != 3)
-            {
-            Label1: Console.WriteLine("Enter no. of rows of second matrix");
-                int rows_secondMatrix = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter no. of columns of second matrix");
-                int columns_secondMatrix = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("The entered matrix is: ");
+            //Matrix.DisplayMatrix(firstMatrix, rows_firstMatrix, columns_firstMatrix);
+            ////Displays entered matrix.
 
-                if (choice == 1 && (rows_firstMatrix != rows_secondMatrix || columns_secondMatrix != columns_firstMatrix))
-                {
-                    Console.WriteLine("Invalid rows or columns\n");
-                    goto Label1;
-                }
-                else if (choice == 2 && (columns_firstMatrix != rows_secondMatrix))
-                {
-                    Console.WriteLine("Invalid rows or columns\n");
-                    goto Label1;
-                }
-                
-                else
-                {
-                    secondMatrix = new int[rows_secondMatrix, columns_secondMatrix];
-                    Console.WriteLine("Enter elements");
-                    for (int i = 0; i < rows_secondMatrix; i++)
-                    {
-                        for (int j = 0; j < columns_secondMatrix; j++)
-                        {
-                            secondMatrix[i, j] = Convert.ToInt32(Console.ReadLine());
-                        }
-                    }
-                }
+            //if (choice != 3)
+            //{
+            //Label1: Console.WriteLine("Enter no. of rows of second matrix");
+            //    int rows_secondMatrix = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Enter no. of columns of second matrix");
+            //    int columns_secondMatrix = Convert.ToInt32(Console.ReadLine());
+            //    //Gets rows and columns.
 
-                if (choice == 1)
-                {
-                    Console.WriteLine("Sum:");
-                    int[,] sum = new int[rows_firstMatrix, columns_firstMatrix];
-                    for (int i = 0; i < rows_firstMatrix; i++)
-                    {
-                        for (int j = 0; j < columns_firstMatrix; j++)
-                        {
-                            sum[i, j] = firstMatrix[i, j] + secondMatrix[i, j];
-                        }
-                    }
+            //    if (choice == 1 && (rows_firstMatrix != rows_secondMatrix || columns_secondMatrix != columns_firstMatrix))
+            //    {
+            //        Console.WriteLine("Invalid rows or columns\n");
+            //        goto Label1;
+            //    }
+            //    //Validation for addition.
+            //    else if (choice == 2 && (columns_firstMatrix != rows_secondMatrix))
+            //    {
+            //        Console.WriteLine("Invalid rows or columns\n");
+            //        goto Label1;
+            //    }   
+            //    //Validation for multiplication
+            //    else
+            //    {
+            //        secondMatrix = new int[rows_secondMatrix, columns_secondMatrix];                    
+            //        secondMatrix = Matrix.GetMatrix(secondMatrix, rows_secondMatrix, columns_secondMatrix);
+            //        //Gets matrix.
+            //        Console.WriteLine("Entered matrix is: ");                    
+            //        Matrix.DisplayMatrix(secondMatrix, rows_secondMatrix, columns_secondMatrix);
+            //        //Displays entered matrix.
+            //    }
 
-                    for (int i = 0; i < rows_firstMatrix; i++)
-                    {
-                        for (int j = 0; j < columns_firstMatrix; j++)
-                        {
-                            Console.Write($"{sum[i, j]} ");
-                        }
-                        Console.WriteLine();
-                    }
-                }
+            //    if (choice == 1)
+            //    {
+            //        int[,] sum = new int[rows_firstMatrix, columns_firstMatrix];
+            //        sum = Matrix.FindSum(sum, firstMatrix, secondMatrix, rows_firstMatrix, columns_firstMatrix);
+            //        //Performs addition.
+            //        Console.WriteLine("Sum:");                    
+            //        Matrix.DisplayMatrix(sum, rows_firstMatrix, columns_firstMatrix);
+            //        //Displays sum.
+            //    }
 
-                if (choice == 2)
-                {
-                    Console.WriteLine("Product:");
-                    int[,] product = new int[rows_firstMatrix, columns_secondMatrix];
-                    int temp = 0;
-                    for (int i = 0; i < rows_firstMatrix; i++)
-                    {
-                        for (int j = 0; j < columns_secondMatrix; j++)
-                        {
-                            for (int k = 0; k < columns_firstMatrix; k++)
-                            {
-                                temp = temp + firstMatrix[i, k] * secondMatrix[k, j];
-                            }
-                            product[i, j] = temp;
-                            temp = 0;
-                        }
-                    }
-                    for (int i = 0; i < rows_firstMatrix; i++)
-                    {
-                        for (int j = 0; j < columns_secondMatrix; j++)
-                        {
-                            Console.Write($"{product[i, j]} ");
-                        }
-                        Console.WriteLine();
-                    }
-                }
-            }
-            else
-            {
-                int[,] transpose = new int[columns_firstMatrix,rows_firstMatrix];
-                Console.WriteLine("Transpose is: ");
-                for (int i = 0; i < rows_firstMatrix; i++)
-                {
-                    for (int j = 0; j < columns_firstMatrix; j++)
-                    {
-                        transpose[j, i] = firstMatrix[i,j];
-                    }
-                }
+            //    if (choice == 2)
+            //    {
+            //        int[,] product = new int[rows_firstMatrix, columns_secondMatrix];
+            //        product = Matrix.FindProduct(product, firstMatrix, secondMatrix, rows_firstMatrix,
+            //                                     columns_secondMatrix, columns_firstMatrix);
+            //        //Performs multiplication.
+            //        Console.WriteLine("Product:");                    
+            //        Matrix.DisplayMatrix(product, rows_firstMatrix, columns_secondMatrix);
+            //        //Displays product.
+            //    }
+            //}
+            //else
+            //{
+            //    int[,] transpose = new int[columns_firstMatrix,rows_firstMatrix];
+            //    Matrix.FindTranspose(transpose, firstMatrix, rows_firstMatrix, columns_firstMatrix);
+            //    //Finds transpose.
+            //    Console.WriteLine("Transpose is: ");                
+            //    Matrix.DisplayMatrix(transpose, columns_firstMatrix, rows_firstMatrix);
+            //    //Displays transpose.
+            //}
 
-                for (int i = 0; i < columns_firstMatrix; i++)
-                {
-                    for (int j = 0; j < rows_firstMatrix; j++)
-                    {
-                        Console.Write($"{transpose[i, j]} ");
-                    }
-                    Console.WriteLine();
-                }
+            //Console.ReadLine();
 
-            }
+            //-------------------------------------------------------------------------------------------------//
 
-            Console.ReadLine();
         }
     }
 }
